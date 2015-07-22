98254e83760ef29200ee2e0849182a43cb5ef5e9
Isabella Santos <isabella.santos@zaez.net>
2015-07-22 13:58:08 -0300

    Change the picture in tests

--- lib/spree_zaez_banner/factories.rb
+++ lib/spree_zaez_banner/factories.rb
@@ -3,6 +3,6 @@ FactoryGirl.define do
     category 'home'
     url 'http://localhost:3000'
     enabled true
-    attachment { File.new(Spree::Core::Engine.root + 'spec/fixtures' + 'thinking-cat.jpg') }
+    attachment { File.new(Rails.root + '../../spec/fixtures/spree.jpg') }
   end
 end
